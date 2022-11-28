cad813ff165f5a8828e96568b5d100c9afb378cc
Peter Mwansa <mwansapeter800@gmail.com>
2022-11-28 16:37:12 +0300

    Make the app more secure by implementing the the post method

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
-    name = request.args.get("name")
+    name = request.form.get("name", "World")
     return render_template("greet.html", name=name)
+
